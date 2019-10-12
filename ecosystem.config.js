df00d8a3a035426aa8a8079c774e3f5fd15ae8ce
alpaca623 <alpaca623@gmail.com>
2019-10-12 23:22:58 +0900

    add pm2 config js
    - ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  apps: [
+    {
+      out_file: '/data/logs/pm2/kdax-out.log',
+      error_file: '/data/logs/pm2/kdax-error.log',
+      name: 'lunch_server',
+      script: './src/init.js',
+      watch: false,
+      log_date_format: 'YYYY-MM-DDTHH:mm:ss:SSSZ',
+      wait_ready: true,
+      listen_timeout: 30000,
+      kill_timeout: 5000,
+      env: {
+        NODE_ENV: 'development',
+        TZ: 'Asia/Seoul',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        TZ: 'Asia/Seoul',
+      },
+      env_stage: {
+        NODE_ENV: 'stage',
+        TZ: 'Asia/Seoul',
+      },
+    },
+  ],
+  deploy: {
+    dev: {
+      ref: 'origin/master',
+      repo: 'http://github.com/alpaca623/lunch-server.git',
+      // path: '',
+      'post-deploy': 'npm install && npm run compile && pm2 reload ecosystem.config.js',
+    },
+    production: {
+      ref: 'origin/master',
+      repo: 'http://github.com/alpaca623/lunch-server.git',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.json --env production',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  },
+};
