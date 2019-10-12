5aff92c7736c5481a74d02b95b150b4369d6c2ed
alpaca623 <alpaca623@gmail.com>
2019-10-12 23:36:20 +0900

    update init.js source
    - change mongodb logic(db.js -> init.js)

--- src/init.js
+++ src/init.js
@@ -1,8 +1,18 @@
-import "./db";
 import app from "./server";
-
+import config from "./config";
+import mongoose from "mongoose";
 import config from "./config";
 
 const PORT = process.env.PORT || config.PORT;
+
+mongoose.connect(process.env.MONGO_URL || config.MONGO_URL, {
+  useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => console.log("✅ db open!!"));
+
+db.on("error", () => console.log("✅ db error!!"));
 
 app.listen(PORT, () => console.log("✅ open lunch server!!"));
